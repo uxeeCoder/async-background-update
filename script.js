6423283515b49511f78f8b6658a96f0039840f6f
uxeeCoder <umar.afar@aol.com>
2024-02-15 00:26:29 -0500

    code commented console.log

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class AsyncDelay {
 const time = 5000;
 const body = document.querySelector("body");
 const wait = new AsyncDelay(time);
-console.log(wait.delay(time));
+// console.log(wait.delay(time));
 wait.delay().then(() => {
   body.style.backgroundColor = "blue";
   wait.delay().then(() => {
